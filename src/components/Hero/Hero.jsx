8257dee11b2af034f183c7c84dda293595ef990c
am224ut <94832146+Albion007@users.noreply.github.com>
2023-11-02 21:01:18 +0100

    changes at hero and header

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -14,14 +14,12 @@ const Hero = () => {
       >
         <div className={css.upperElements}>
           <motion.span className="primaryText" variants={fadeIn("right", "tween", 0.2, 1)}>
-            Hey There,
+            Hi,everyone!
             <br />
-            I'm Binjan.
+            I'm Albion Morina.
           </motion.span>
           <motion.span className="secondaryText"variants={fadeIn("left", "tween", 0.4, 1)}>
-            I design beautiful simple
-            <br />
-            things, And I love what i do{" "}
+            Software Developer and IT Support{" "}
           </motion.span>
         </div>
 
@@ -29,26 +27,26 @@ const Hero = () => {
           variants={fadeIn("up", "tween", 0.3, 1)}
           className={css.person}
         >
-          <motion.img variants={slideIn("up", "tween", 0.5, 1.3)} src="./person.png" alt="" />
+          <motion.img variants={slideIn("up", "tween", 0.5, 1.3)} src="./1.png" alt="" />
         </motion.div>
 
-        <a className={css.email} href="mailto:zainkeepscode@gmail.com">
-          zainkeepscode@gmail.com
+        <a className={css.email} href="mailto:albionmorina44@gmail.com">
+          albionmorina44@gmail.com
         </a>
 
         <div className={css.lowerElements}>
           <motion.div variants={fadeIn("right", "tween", 0.3, 1)} className={css.experience}>
-            <div className="primaryText">10</div>
+            <div className="primaryText">2+</div>
             <div className="secondaryText">
-              <div>Years</div>
-              <div>Experience</div>
+              <div>years</div>
+              <div>experience</div>
             </div>
           </motion.div>
 
           <motion.div variants={fadeIn("left", "tween", 0.5, 1)} className={css.certificate}>
             <img src="./certificate.png" alt="" />
-            <span>CERTIFIED PROFATIONAL</span>
-            <span>UI/UX DESIGNER</span>
+            <span>Software Design</span>
+            <span>Social Media and Web Technologies</span>
           </motion.div>
         </div>
       </motion.div>
